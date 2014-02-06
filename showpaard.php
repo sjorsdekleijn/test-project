5e1dcf8074b80c0242947180db07ef60a7c57f29
sjorsdekleijn <sjors@webs.nl>
2014-02-06 13:18:33 +0000

    sttylesheet toegevoegd

--- showpaard.php
+++ showpaard.php
@@ -1,12 +1,19 @@
+<html>
+  <head>
+    <title>Dit is mijn paard</title>
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+  <body>
 <?php
 
-/*
- * To change this template use Tools | Templates.
- */
-include('paard.php');
+  /*
+   * To change this template use Tools | Templates.
+   */
+  include('paard.php');
+  
+  /* PAARDEN TRUUK */
+  $paard = new paard();
 
-/* PAARDEN TRUUK */
-$paard = new paard( TRUE );
-$paard->setPaard( "FRANS" );
-echo $paard->getPaard();
 ?>
+  </body>
+</html>
